22388ebe8642d8a295acc90b7703db5d2632188f
Mustafa Safri <msafri@clearpathrobotics.com>
2015-05-06 12:27:43 -0400

    Fixed startup publishing issue

--- src/LMS1xx.cpp
+++ src/LMS1xx.cpp
@@ -21,6 +21,7 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -30,6 +31,7 @@
 #include <cstring>
 #include <unistd.h>
 
+#include "ros/ros.h"
 #include "LMS1xx/LMS1xx.h"
 #include "console_bridge/console.h"
 
@@ -50,7 +52,6 @@ void LMS1xx::connect(std::string host, int port) {
 			stSockAddr.sin_family = PF_INET;
 			stSockAddr.sin_port = htons(port);
 			Res = inet_pton(AF_INET, host.c_str(), &stSockAddr.sin_addr);
-
 			int ret = ::connect(sockDesc, (struct sockaddr *) &stSockAddr,
 					sizeof stSockAddr);
 			if (ret == 0) {
@@ -117,9 +118,24 @@ status_t LMS1xx::queryStatus() {
 
 void LMS1xx::login() {
 	char buf[100];
+	int result;
 	sprintf(buf, "%c%s%c", 0x02, "sMN SetAccessMode 03 F4724744", 0x03);
+	
+	fd_set readset;
+	struct timeval timeout;
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
 
-	write(sockDesc, buf, strlen(buf));
+	do { //loop until data is available to read
+	  ros::Duration(1.0).sleep();
+
+	  write(sockDesc, buf, strlen(buf));
+
+	  FD_ZERO(&readset);
+	  FD_SET(sockDesc, &readset);
+	  result = select(sockDesc + 1, &readset, NULL, NULL, &timeout);
+
+	} while (result <= 0);
 
 	int len = read(sockDesc, buf, 100);
 	if (buf[0] != 0x02)
